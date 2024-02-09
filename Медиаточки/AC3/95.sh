4b5e7ad84ac302a929c875b66c5865133585638c
Frame300 <142489429+Frame300@users.noreply.github.com>
2024-02-09 17:30:02 +0300

    Create 95.sh

---
+++ Медиаточки/AC3/95.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/php
+<?php
+//Парсинг
+function parser($text){
+    $string=[
+        "addr"=>"",
+        "command" => "",
+        "fake" => "",
+    ];
+    $helper = [
+        "0" => "",
+        "1" => "",
+        "2" => "",
+    ];
+    echo "String_getted = ". $text."\n";
+    $helper = preg_split("/[*|*]/",$text);
+    $string["addr"] = substr($helper[0],0);
+    $string["command"] = substr($helper[1],0);
+    $string["fake"] = substr($helper[2],5);
+    //var_dump($string);
+    return $string;
+}
+//Получение параметров
+$consoleParam = hex2bin($argv[3]);
+$consoleParam = parser($consoleParam);
+//Передача запроса
+$url = "http://". $consoleParam['addr'] . "/" . $consoleParam['command'];
+echo $url; 
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url); 
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+$result=curl_exec($ch);
+echo $result; 
+curl_close($ch);
+?>
