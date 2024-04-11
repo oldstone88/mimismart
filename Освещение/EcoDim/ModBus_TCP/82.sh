0f38e56a3158daff2b99fabacf9165a9911a4b03
Frame300 <142489429+Frame300@users.noreply.github.com>
2024-04-11 14:16:04 +0300

    logging

--- Освещение/EcoDim/ModBus_TCP/82.sh
+++ Освещение/EcoDim/ModBus_TCP/82.sh
@@ -8,6 +8,7 @@ try:
     data = result_string.split("|")
     c = ModbusClient(host=data[0], port=502, unit_id=1, auto_open=True)
     c.write_single_register(3000+int(data[1])*5, int(data[2][:data[2].find("\x00")]))
+    print(f"Changed state of EcoDim device - id: {data[1]} brightness: {data[2]}")
 except Exception as ex:
     print("Usage: script.sh <path> <path> <id:sid> <№device 0-63> <brightess value 0-250>")
     print(ex)
