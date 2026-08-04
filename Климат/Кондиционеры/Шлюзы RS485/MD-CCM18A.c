4e9afc50da581542c0908259e4bbc25650e9960d
Sergey Kamenskiy <72188575+oldstone88@users.noreply.github.com>
2026-08-04 15:54:07 +0300

    Update MD-CCM18A.c

--- Климат/Кондиционеры/Шлюзы RS485/MD-CCM18A.c
+++ Климат/Кондиционеры/Шлюзы RS485/MD-CCM18A.c
@@ -9,6 +9,7 @@
     {tag:"item", id:"%TARGET%", name:"Кондиционер 2", "sub-id":"%SUBID03%", "type":"conditioner", "t-min":"17", "t-delta":"13", "vane-ver":"0x00", "vane-hor":"0x00", "funs":"0x0F", "modes":"0x1F"},
     {tag:"item", id:"%TARGET%", name:"Кондиционер 3", "sub-id":"%SUBID05%", "type":"conditioner", "t-min":"17", "t-delta":"13", "vane-ver":"0x00", "vane-hor":"0x00", "funs":"0x0F", "modes":"0x1F"},
     {tag:"item", id:"%TARGET%", name:"Кондиционер 4", "sub-id":"%SUBID07%", "type":"conditioner", "t-min":"17", "t-delta":"13", "vane-ver":"0x00", "vane-hor":"0x00", "funs":"0x0F", "modes":"0x1F"},
+    {tag:"item", id:"%TARGET%", name:"Кондиционер 5", "sub-id":"%SUBID09%", "type":"conditioner", "t-min":"17", "t-delta":"13", "vane-ver":"0x00", "vane-hor":"0x00", "funs":"0x0F", "modes":"0x1F"},
   ],
   vars:[
         {name:"RS485",type:"devices-list",required:true,filter:["com-port"],desc:"RS485"},
@@ -17,6 +18,7 @@
         {name:"COND01",type:"hidden",value:"%TARGET%:%SUBID03%"},
         {name:"COND02",type:"hidden",value:"%TARGET%:%SUBID05%"},
         {name:"COND03",type:"hidden",value:"%TARGET%:%SUBID07%"},
+        {name:"COND04",type:"hidden",value:"%TARGET%:%SUBID09%"},
   ]
 }
 */
@@ -26,12 +28,14 @@ const u16 ID [] = {
     #ifdef COND01, ADDR2ID(COND01) #else ,0 #endif
     #ifdef COND02, ADDR2ID(COND02) #else ,0 #endif
     #ifdef COND03, ADDR2ID(COND03) #else ,0 #endif
+    #ifdef COND04, ADDR2ID(COND04) #else ,0 #endif
 };
 const u8 SID [] = {
     ADDR2SID(COND00)
     #ifdef COND01, ADDR2SID(COND01) #else ,0 #endif
     #ifdef COND02, ADDR2SID(COND02) #else ,0 #endif
     #ifdef COND03, ADDR2SID(COND03) #else ,0 #endif
+    #ifdef COND04, ADDR2SID(COND04) #else ,0 #endif
 };
 
 u8 cond[5] = {0, 0, 0, 0, 0};
@@ -49,13 +53,14 @@ void Send(){
     breakpoint = 0;
     cancelDelayedCall(Send);
     if(NeedSend){
-        for(u8 i = 0; (i < 4) && (breakpoint ==0 ); ++i){
+        for(u8 i = 0; (i < 5) && (breakpoint == 0 ); ++i){
             if((NeedSend >> i)&1){
                 breakpoint = 1;
                 if(!i){getStatus(COND00, cond);}
                 else if(i == 1){getStatus(COND01, cond);}
                 else if(i == 2){getStatus(COND02, cond);}
                 else if(i == 3){getStatus(COND03, cond);}
+                else if(i == 4){getStatus(COND04, cond);}
                 modbusregister = (32 * i) + 1; // Вычисляется адрес первого регистра для записи
                 condstatus[2] = modbusregister>>8;
                 condstatus[3] = modbusregister;
@@ -87,9 +92,9 @@ void Send(){
     }
 }
 
-V-ID/COND00, COND01, COND02, COND03{
+V-ID/COND00, COND01, COND02, COND03, COND04{
     if(senderId()!=exciterId()){
-        for(u8 i=0; i<4; ++i){
+        for(u8 i=0; i<5; ++i){
             if(SID[i] && SID[i]==senderSubId()){
                     write=1;
                     NeedSend |= 1<<i;
@@ -106,7 +111,7 @@ u8 readcond[8] = {Address, 0x03, 0, 0, 0x00, 0x03, 0xCC, 0x16};
 V-ID/s:1{
     if(write == 0){
         ++readCount;
-        if(readCount>3) readCount=0;
+        if(readCount>4) readCount=0;
         modbusregister = (readcond * 32) + 1; // Вычисляется адрес первого регистра для чтения
         readcond[2] = modbusregister>>8;
         readcond[3] = modbusregister;
@@ -150,6 +155,7 @@ void synccond(){
     else if(readCount == 1) setStatus(COND01, &cond);
     else if(readCount == 2) setStatus(COND02, &cond);
     else if(readCount == 3) setStatus(COND03, &cond);
+    else if(readCount == 4) setStatus(COND04, &cond);
 }
 
 V-ID/RS485{
